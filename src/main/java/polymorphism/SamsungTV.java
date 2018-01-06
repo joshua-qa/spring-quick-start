2b3eb0e3bde2a3eca8498302b71367fdf6b76949
joshua-qa <jgchoi.qa@gmail.com>
2018-01-06 16:47:51 +0900

    생성자 인젝션 이용하기까지 실습 진행 (79p)

--- src/main/java/polymorphism/SamsungTV.java
+++ src/main/java/polymorphism/SamsungTV.java
@@ -1,6 +1,9 @@
 package polymorphism;
 
 public class SamsungTV implements TV {
+    private Speaker speaker;
+    private int price;
+
     public void initMethod() {
         System.out.println("객체 초기화 작업 처리..");
     }
@@ -13,8 +16,19 @@ public class SamsungTV implements TV {
         System.out.println("===> SamsungTV 객체 생성");
     }
 
+    public SamsungTV(Speaker speaker) {
+        System.out.println("===> SamsungTV(2) 객체 생성");
+        this.speaker = speaker;
+    }
+
+    public SamsungTV(Speaker speaker, int price) {
+        System.out.println("===> SamsungTV(3) 객체 생성");
+        this.speaker = speaker;
+        this.price = price;
+    }
+
     public void powerOn() {
-        System.out.println("SamsungTV---전원 켠다.");
+        System.out.println("SamsungTV---전원 켠다. (가격 : " + price + ")");
     }
 
     public void powerOff() {
@@ -22,10 +36,10 @@ public class SamsungTV implements TV {
     }
 
     public void volumeUp() {
-        System.out.println("SamsungTV---소리 올린다.");
+        speaker.volumeUp();
     }
 
     public void volumeDown() {
-        System.out.println("SamsungTV---소리 내린다.");
+        speaker.volumeDown();
     }
 }
--- src/main/java/polymorphism/TVUser.java
+++ src/main/java/polymorphism/TVUser.java
@@ -9,6 +9,7 @@ public class TVUser {
         AbstractApplicationContext factory = new GenericXmlApplicationContext("applicationContext.xml");
 
         // 2. Spring 컨테이너로부터 필요한 객체를 요청(Lookup) 한다.
+        // bean scope 설정을 prototype 으로 지정하면 해당 bean이 요청될 때마다 매번 새로운 객체를 생성해서 반환한다.
         TV tv = (TV)factory.getBean("tv"); // initMethod 호출
         tv.powerOn();
         tv.volumeUp();
